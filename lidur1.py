0f4a1dab4b3883759b7ff254c1883bf2ca6fe44d
atli164 <atli164@users.noreply.github.com>
2019-02-16 16:58:02 +0000

    Update lidur1.py

--- lidur1.py
+++ lidur1.py
@@ -11,3 +11,19 @@ def f(th):
     N2 = A2*(p3**2-p1**2-A3**2-B3**2)
     N2 -= A3*(p2**2-p1**2-A2**2-B2**2)
     return N1**2+N2**2-p1**2*D**2
+
+def g(x1, x2, y2, L1, L2, L3, gam, p1, p2, p3):
+    def f(th):
+        A2 = L3*cos(th)-x1
+        B2 = L3*sin(th)
+        A3 = L2*cos(th+gam) - x2
+        B3 = L2*sin(th+gam) - y2
+        D = 2*(A2*B3-B2*A3)
+        if D == 0:
+            raise ValueError("D is 0")
+        N1 = B3*(p2**2-p1**2-A2**2-B2**2)
+        N1 -= B2*(p3**2-p1**2-A3**2-B3**2)
+        N2 = A2*(p3**2-p1**2-A3**2-B3**2)
+        N2 -= A3*(p2**2-p1**2-A2**2-B2**2)
+        return N1**2+N2**2-p1**2*D**2
+    return f
